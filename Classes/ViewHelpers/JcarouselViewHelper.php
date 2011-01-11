871040645f5ed1edbfd1af012b2952c24dbe4077
Fernando Arconada <fernando.arconada@gmail.com>
2011-01-11 20:48:39 +0100

    CSS inline en funcion del tamaño

--- Classes/ViewHelpers/JcarouselViewHelper.php
+++ Classes/ViewHelpers/JcarouselViewHelper.php
@@ -9,11 +9,17 @@ class Tx_f2jcarouselvh_ViewHelpers_JcarouselViewHelper extends Tx_Fluid_Core_Vie
 	 * @param string $jsonJCarouselConfig
 	 * @param string $preJsCode
 	 * @param string $postJsCode
+	 * @param integer $itemHeight
+	 * @param integer $itemWidth
+	 * @param integer $containerHeight
+	 * @param integer $containerWidth
 	 * @return string
 	 */
-	public function render($htmlItems = NULL, $cssFile = NULL, $jsonJCarouselConfig = NULL, $preJsCode = NULL, $postJsCode) {
+	public function render($htmlItems = NULL, $cssFile = NULL, $jsonJCarouselConfig = NULL, $preJsCode = NULL, $postJsCode, $itemHeight = 75, $itemWidth = 75, $containerWidth = 300, $containerHeight = 100) {
 		$this->addHeaderFiles($cssFile);
-
+		if (!$cssFile) {
+			$this->addHeaderStyle($itemHeight, $itemWidth, $containerWidth, $containerHeight);
+		}
 		$pluginId = md5(time());
 
 		$this->addJcarouselJS($pluginId, $jsonJCarouselConfig, $preJsCode, $postJsCode);
@@ -43,6 +49,37 @@ class Tx_f2jcarouselvh_ViewHelpers_JcarouselViewHelper extends Tx_Fluid_Core_Vie
 					'<link rel="stylesheet" type="text/css" href="'. t3lib_extMgm::extRelPath('f2jcarouselvh') . 'Resources/Public/StyleSheet/skins/tango/skin.css" />'
 				);
 			}
+		}
+	}
+
+	private function addHeaderStyle($itemHeight = 75, $itemWidth = 75, $containerWidth = 300, $containerHeight = 100){
+		if ( !isset($GLOBALS['f2jcarouselvh_css_inline'])) {
+			$response = $this->controllerContext->getResponse();
+			$GLOBALS['f2jcarouselvh_css_inline'] = TRUE;
+			$response->addAdditionalHeaderData('
+				<style>
+					.jcarousel-skin-tango .jcarousel-item {
+							height: '. $itemHeight .'px;
+							width: '. $itemWidth .'px;
+					}
+					.jcarousel-skin-tango .jcarousel-container-vertical {
+							height: '. $containerHeight .'px;
+							width: '. $containerWidth .'px;
+					}
+					.jcarousel-skin-tango .jcarousel-clip-vertical {
+							height: '. $containerHeight .'px;
+							width: '. $containerWidth .'px;
+					}
+					.jcarousel-skin-tango .jcarousel-container-horizontal {
+							height: '. $containerHeight .'px;
+							width: '. $containerWidth .'px;
+					}
+					.jcarousel-skin-tango .jcarousel-clip-horizontal {
+							height: '. $containerHeight .'px;
+							width: '. $containerWidth .'px;
+					}
+				</style>
+			');
 		}
 	}
 
